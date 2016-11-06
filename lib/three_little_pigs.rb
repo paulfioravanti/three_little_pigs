e098e9e069075dfc60aef72ffcefbb34632ce757
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-11-06 23:59:35 +1100

    Add binary file that will run the program

--- lib/three_little_pigs.rb
+++ lib/three_little_pigs.rb
@@ -1,3 +1,4 @@
+require "forwardable"
 # NOTE: `require_relative` done here so that the `Part` class is loaded
 # before any of the other house parts that reference it.
 require_relative "three_little_pigs/objects/house/part"
