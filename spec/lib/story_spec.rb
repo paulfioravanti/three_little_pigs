56e5c100861e3e78a4d6a64c7eaa895116831388
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-11-07 19:39:01 +1100

    Flesh out full story spec

--- spec/lib/story_spec.rb
+++ spec/lib/story_spec.rb
@@ -3,14 +3,26 @@ require "spec_helper"
 module ThreeLittlePigs
   RSpec.describe Story do
     describe ".tell" do
+      let(:chapters) { Chapters.all }
+
       before do
-        allow(Chapters::OnceUponATime).to receive(:tell)
+        chapters.each do |chapter, text|
+          allow(chapter).to receive(:tell)
+          allow(described_class).to receive(:puts).with(green(text))
+        end
+        described_class.tell
       end
 
-      it "tells the story" do
-        described_class.tell
-        expect(Chapters::OnceUponATime).to have_received(:tell)
+      it "tells the whole story" do
+        chapters.each do |chapter, text|
+          expect(chapter).to have_received(:tell)
+          expect(described_class).to have_received(:puts).with(green(text))
+        end
       end
+    end
+
+    def green(text)
+      "\e[32m#{text}\e[0m"
     end
   end
 end
