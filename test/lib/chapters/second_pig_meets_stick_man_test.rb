0355b41862f185cc83d8d2f08419f87255b684e4
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-11-08 08:54:59 +1100

    Finish second pig tests

---
+++ test/lib/chapters/second_pig_meets_stick_man_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+module ThreeLittlePigs
+  module Chapters
+    class SecondPigMeetsStickManTest < Minitest::Test
+      attr_reader :story, :stick_man
+
+      def setup
+        suppress_output do
+          @story = Story.so_far(until_chapter: SecondPigMeetsStickMan)
+          SecondPigMeetsStickMan.tell(story)
+        end
+        @stick_man = story.stick_man
+      end
+
+      def test_the_second_pig_met_a_man_carrying_a_bundle_of_sticks
+        assert_kind_of(Man, stick_man)
+        assert_equal(Bundle.of(:sticks), stick_man.inventory)
+      end
+    end
+  end
+end
