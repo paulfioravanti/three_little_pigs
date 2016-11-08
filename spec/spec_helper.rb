7662b41af2a793464af194e131bcb5c9f0865aa4
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-11-08 23:41:28 +1100

    Add callout to codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require "simplecov"
+if ENV["TRAVIS"]
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
 require "three_little_pigs"
 require "pry-byebug"
+
 
 RSpec.configure do |config|
   config.before { allow($stdout).to receive(:puts) }
