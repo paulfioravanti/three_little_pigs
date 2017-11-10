2c59b3c0ffbfc5ccc883c8b6dddf2dbc2586539b
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-11-10 17:04:37 +1100

    Remove commented out code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,16 +4,6 @@ require "simplecov"
 require "three_little_pigs"
 require "pry-byebug"
 
-# unless ENV["NO_COVERAGE"]
-#   SimpleCov.command_name("RSpec specs")
-
-#   unless SimpleCov.running
-#     SimpleCov.start do
-#       SimpleCov.minimum_coverage 100
-#     end
-#   end
-# end
-
 RSpec.configure do |config|
   config.filter_run(focus: true)
   config.run_all_when_everything_filtered = true
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,16 +8,6 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 require "pry-byebug"
 
-# unless ENV["NO_COVERAGE"]
-#   SimpleCov.command_name("Minitest tests")
-
-#   unless SimpleCov.running
-#     SimpleCov.start do
-#       SimpleCov.minimum_coverage 100
-#     end
-#   end
-# end
-
 # REF: https://gist.github.com/moertel/11091573
 # rubocop:disable Lint/RescueException
 # NOTE: This method only temporarily rescues from Exception before re-raising.
