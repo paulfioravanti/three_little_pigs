edb8c67286040048614042b034c690f179ce6787
Paul Fioravanti <paul.fioravanti@gmail.com>
2021-02-01 08:29:39 +1100

    Fix Rubocop issue

--- spec/lib/chapters/chapter_03_pigs_leave_the_house_spec.rb
+++ spec/lib/chapters/chapter_03_pigs_leave_the_house_spec.rb
@@ -11,7 +11,7 @@ module ThreeLittlePigs
       before { described_class.tell(story) }
 
       specify "the mother pig did not have enough to keep her children" do
-        expect(mother_pig.wealth).to_not eq(Wealth.level(:enough))
+        expect(mother_pig.wealth).not_to eq(Wealth.level(:enough))
       end
 
       specify "the mother pig sent the three little pigs away" do
